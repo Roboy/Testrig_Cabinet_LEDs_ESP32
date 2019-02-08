406ddc710e5c4eafe56a5f57c98bb87737a69039
DerGenaue <dergenaue@online.de>
2019-02-09 00:51:42 +0100

    ESP32 board setup & FastLED reel 100 demo

---
+++ include/main.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+
+void setup();
+void loop();
+
+void nextPattern();
+void rainbow();
+void rainbowWithGlitter();
+void addGlitter( fract8 chanceOfGlitter);
+void confetti();
+void sinelon();
+void bpm();
+void juggle();
